aa821576b820febc6e907bbc047dcadd55d890d5
leonardoffirmino <firmino750@gmail.com>
2024-02-14 16:30:10 -0300

    Dialog content

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,10 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: "Hello World"
+}
 
 export function App() {
   return (
@@ -23,12 +27,7 @@ export function App() {
 
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-
+        <NoteCard note={note} />
 
       </div>
 
